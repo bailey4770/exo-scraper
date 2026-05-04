156749800dac42112389f04ae4712972930dcfed
Bailey Stringer <bailey4770@protonmail.com>
2026-05-04 16:47:59 +0100

    fix(clean): fix transit_duration bug and clean file

--- clean.py
+++ clean.py
@@ -2,9 +2,24 @@ import pandas as pd
 import numpy as np
 
 DATA_FILE_PATH = "./data/exoplanet.eu_catalog_29-04-26_12_09_18.csv"
+RESULT_FILE_PATH = "./data/golden_dataset.csv"
+
+TEST_STARS: list[str] = ["Kepler-69"]
+RETURN_COLS: list[str] = [
+    "star_name",
+    "name",
+    "orbital_period_min",
+    "orbital_period",
+    "orbital_period_max",
+    "min_depth",
+    "depth",
+    "max_depth",
+    "transit_duration_min",
+    "transit_duration",
+    "transit_duration_max",
+]
 
 # Need to ensure units are equal when calculating ratios
-
 # Jupiter's radius as a fraction of the Sun
 Rj_SOLAR_RADIUS = 0.10045
 # Sun's radius as a fraction of 1 AU
@@ -12,37 +27,37 @@ Rs_AU = 0.00465
 
 
 def _get_orbital_period_cols(df: pd.DataFrame) -> pd.DataFrame:
+    orbital_period_min = df["orbital_period"] - df["orbital_period_error_min"]
+    orbital_period_max = df["orbital_period"] + df["orbital_period_error_max"]
+
     return df.assign(
-        orbital_period_min=(df["orbital_period"] - df["orbital_period_error_min"]),
-        orbital_period_max=(df["orbital_period"] + df["orbital_period_error_max"]),
+        orbital_period_min=orbital_period_min, orbital_period_max=orbital_period_max
     )
 
 
-def _get_depth_cols(df: pd.DataFrame):
-    res: pd.DataFrame = df.assign(
-        radius_min=(df["radius"] - df["radius_error_min"]),
-        radius_max=(df["radius"] + df["radius_error_max"]),
-        star_radius_min=(df["star_radius"] - df["star_radius_error_min"]),
-        star_radius_max=(df["star_radius"] + df["star_radius_error_max"]),
-    )
-    res["min_depth"] = (
-        res["radius_min"] * Rj_SOLAR_RADIUS / res["star_radius_max"]
-    ) ** 2
-    res["max_depth"] = (
-        res["radius_max"] * Rj_SOLAR_RADIUS / res["star_radius_min"]
-    ) ** 2
-    return res
+def _get_depth_cols(df: pd.DataFrame) -> pd.DataFrame:
+    radius_min = df["radius"] - df["radius_error_min"]
+    radius_max = df["radius"] + df["radius_error_max"]
+    star_radius_min = df["star_radius"] - df["star_radius_error_min"]
+    star_radius_max = df["star_radius"] + df["star_radius_error_max"]
+
+    min_depth = (radius_min * Rj_SOLAR_RADIUS / star_radius_max) ** 2
+    depth = (df["radius"] * Rj_SOLAR_RADIUS / df["star_radius"]) ** 2
+    max_depth = (radius_max * Rj_SOLAR_RADIUS / star_radius_min) ** 2
+    return df.assign(min_depth=min_depth, depth=depth, max_depth=max_depth)
 
 
 def _get_transit_duration(df: pd.DataFrame) -> pd.DataFrame:
     """T = (orbital_period / π) * arcsin(
         (star_radius / semi_major_axis) * sqrt((1 + (radius / star_radius)² - impact_parameter²)
-    )
-        - increasing in orbital period
-        - increasing in star_radius
-        - decreasing in semi_major_axis
-        - increasing in radius
-        - decreasing in impact_parameter
+    ) (Source: Claude -> Winn (2010))
+
+    Variables are (source: own desmos analysis):
+    - increasing in orbital period
+    - increasing in star_radius
+    - decreasing in semi_major_axis
+    - increasing in radius
+    - decreasing in impact_parameter
     """
 
     def _get_arcsin_input(
@@ -60,7 +75,8 @@ def _get_transit_duration(df: pd.DataFrame) -> pd.DataFrame:
             )
         )
 
-        arcsin_input = arcsin_input.dropna()
+        # assert because we have a real problem if this doesn't hold
+        # also, this is a one time script, not a repeatable pipeline
         assert arcsin_input.between(0, 1).all()
         return arcsin_input
 
@@ -70,9 +86,9 @@ def _get_transit_duration(df: pd.DataFrame) -> pd.DataFrame:
         radius=(df["radius"] - df["radius_error_min"]),
         impact_parameter=(df["impact_parameter"] + df["impact_parameter_error_max"]),
     )
-    transit_duration_min = (df["orbital_period_error_min"] / np.pi) * np.arcsin(
-        arcsin_input_min
-    )
+    transit_duration_min = (
+        (df["orbital_period"] - df["orbital_period_error_min"]) / np.pi
+    ) * np.arcsin(arcsin_input_min)
 
     arcsin_input = _get_arcsin_input(
         star_radius=df["star_radius"],
@@ -88,9 +104,9 @@ def _get_transit_duration(df: pd.DataFrame) -> pd.DataFrame:
         radius=(df["radius"] + df["radius_error_max"]),
         impact_parameter=(df["impact_parameter"] - df["impact_parameter_error_min"]),
     )
-    transit_duration_max = (df["orbital_period_error_max"] / np.pi) * np.arcsin(
-        arcsin_input_max
-    )
+    transit_duration_max = (
+        (df["orbital_period"] + df["orbital_period_error_max"]) / np.pi
+    ) * np.arcsin(arcsin_input_max)
 
     return df.assign(
         transit_duration_min=transit_duration_min,
@@ -101,29 +117,23 @@ def _get_transit_duration(df: pd.DataFrame) -> pd.DataFrame:
 
 def main():
     df: pd.DataFrame = pd.read_csv(DATA_FILE_PATH)
-    # print(df)
-    # print("cols: ", list(df))
+    test_planets = df.loc[df["star_name"].isin(TEST_STARS)]
+    dropped_na = test_planets.dropna(
+        subset=[
+            "orbital_period",
+            "star_radius",
+            "semi_major_axis",
+            "radius",
+            "impact_parameter",
+        ]
+    )
 
-    return_cols: list[str] = [
-        "star_name",
-        "name",
-        "orbital_period_min",
-        "orbital_period_max",
-        "radius",
-        "star_radius",
-        "min_depth",
-        "max_depth",
-        "transit_duration_min",
-        "transit_duration",
-        "transit_duration_max",
-    ]
+    with_orbital_error_cols = _get_orbital_period_cols(dropped_na)
+    with_depth_error_cols = _get_depth_cols(with_orbital_error_cols)
+    with_transit_error_cols = _get_transit_duration(with_depth_error_cols)
 
-    kepler_planets = df.loc[df["star_name"] == "Kepler-69"]
-
-    final = _get_orbital_period_cols(kepler_planets)
-    final = _get_depth_cols(final)
-    final = _get_transit_duration(final)
-    print(final[return_cols])
+    print(with_transit_error_cols[RETURN_COLS])
+    with_transit_error_cols.to_csv(RESULT_FILE_PATH)
 
 
 if __name__ == "__main__":
